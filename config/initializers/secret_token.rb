b3b09de4eb8b1b75c941dc34742562c191830e5a
whitered <dmitry.zhelnin@gmail.com>
2011-10-26 19:02:10 +0400

    initial commit: empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avia::Application.config.secret_token = '93e8e459f09c8fda70a6af5a3a4b27b31b32993bb7e0c10dcc942bfdbec265bd78e1e00208d9021c1574e22fcbcbb7a1b0f041115507e322fc5f62072aa348cd'
